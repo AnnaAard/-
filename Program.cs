fc8ad7dc7b4e7d81f462d6959121eee373db0ebb
Anna Aard <anna.mar00722@gmail.com>
2023-02-28 21:27:03 +0600

    нашла количество нужных элементов

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@
 
 Console.WriteLine("введите количество элементов массива: ");
 int sizeArr = Convert.ToInt32(Console.ReadLine());
-string [] array = new string[sizeArr];
+string[] array = new string[sizeArr];
 string[] arrayString = UserFillArray(array);
 PrintArray(arrayString);
+int countLessThree = Count(arrayString);
+string[] newArray = NewArray(arrayString, countLessThree);
+Console.WriteLine();
+PrintArray(newArray);
+Console.WriteLine($" => Найденных элементов {countLessThree}");
+
 
 
 
@@ -15,7 +21,7 @@ string[] UserFillArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine($"Введите {i+1} элемент массива: ");
+        Console.WriteLine($"Введите {i + 1} элемент массива: ");
         array[i] = Convert.ToString(Console.ReadLine());
     }
     return array;
@@ -23,11 +29,37 @@ string[] UserFillArray(string[] array)
 
 void PrintArray(string[] array)
 {
-Console.Write("[");
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write($"| {array[i]} |");
-}
-Console.Write("]");
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write($" {array[i]},  ");
+        else Console.Write($" {array[i]} ");
+    }
+    Console.Write("]");
 }
 
+int Count(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length > 0 && array[i].Length < 4) count++;
+    }
+    return count;
+}
+
+string[] NewArray(string[] array, int size)
+{
+    string[] newArray = new string[size];
+    int i = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j].Length > 0 && array[j].Length < 4)
+        {
+            newArray[i] = array[j];
+            i++;
+        }
+    }
+    return newArray;
+
+}
